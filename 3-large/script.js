9fcb4c4508e84c9cef2d49b3f59a239fc7960298
Jake Luecke <jrluecke95@gmail.com>
2021-02-09 10:29:56 -0500

    finished medium

--- 3-large/script.js
+++ 3-large/script.js
@@ -1,1 +1,20 @@
-// JS Goes here
+let numArray= []
+
+function numberClick(event) {
+    event.preventDefault();
+    if (event.target.classList.contains('number')) {
+        numArray.push(parseInt(event.target.innerText));
+    }
+}
+
+document.addEventListener('click', numberClick)
+
+let operatorArray= [];
+function operators(event) {
+    event.preventDefault();
+    if (event.target.classList.contains('operator') && numArray.length > 0) {
+        operatorArray.push(event.target.innerText)
+    }
+}
+
+document.addEventListener('click', operators)
